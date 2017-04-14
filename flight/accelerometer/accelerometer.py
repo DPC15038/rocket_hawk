9b968f4736ca68214543533b162ce20e2d859e2f
DPC15038@gmail.com <DPC15038@gmail.com>
2017-04-14 19:44:57 +0000

    Accelerometer also accounts for I2C bus delays

--- flight/accelerometer/accelerometer.py
+++ flight/accelerometer/accelerometer.py
@@ -1,6 +1,6 @@
 # Author: Tony DiCola
 # https://github.com/adafruit/Adafruit_Python_LSM303/blob/master/examples/simpletest.py
-# Modified for UML Rocket Club: David Connolly
+# Modified for UML Rocket Club: David Connolly and Jacob Hempel
 # License: Public Domain
 import time
 import sys
@@ -10,28 +10,20 @@ import Adafruit_LSM303
 
 # Create a LSM303 instance.
 lsm303 = Adafruit_LSM303.LSM303()
-
-# Alternatively you can specify the I2C bus with a bus parameter:
-#lsm303 = Adafruit_LSM303.LSM303(busum=2)
-
+N = 10; # Number of samples per second
 fo = open("/home/pi/rocket_hawk/logs/accelerometer_logs/accelerometer.txt", "a")
-if len(sys.argv) > 1:
-	seconds = [int(i) for i in (sys.argv[1:])][0]* 10
-else:
-	seconds = 10
+if len(sys.argv) > 1:  # If a command line argument is provided, use it
+	seconds = [int(i) for i in (sys.argv[1:])][0]*10
+else:  # else, default to 10 second intervals
+	seconds = 10*N
+
+duration = ((1/float(N)) - .0034) # Off set slightly to account for i2c bus delay
 
 for x in range(0, seconds):
-    # Read the X, Y, Z axis acceleration values and print them.
     accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from the reading and print them out.
     accel_x, accel_y, accel_z = accel
     mag_x, mag_z, mag_y = mag
-   # print('Accel X={0}, Accel Y={1}, Accel Z={2}'.format(
-    #      accel_x, accel_y, accel_z))
     fo.write('Accel X={0}, Accel Y={1}, Accel Z={2}\n'.format(
-          accel_x, accel_y, accel_z))
-
-    # Wait quarter of a second and repeat.
-    time.sleep(0.1)
-
+    accel_x, accel_y, accel_z))
+    time.sleep(duration)
 fo.close()
